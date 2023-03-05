7bd5e89244abec60f87d69e51161aa72346ad448
Adam Spruijt (Mousley) <adamspruijt@gmail.com>
2023-03-04 20:17:59 -0500

    sky shader fixed

--- environment/environment.gd
+++ environment/environment.gd
@@ -6,7 +6,10 @@ const SKY_SIZE = 64.0
 @export var sky_color := Color.DARK_BLUE :
 	set(value):
 		sky_color = value
-		set_sky_color(value)
+		environment.background_color = value
+		%Sky.get_active_material(0).set_shader_parameter("sky_color", value)
+		
+
 @export var ground_color := Color.DARK_SLATE_GRAY : set = set_ground_color
 @export var fog_color := Color.CADET_BLUE : set = set_fog_color
 @export var horizon_color := Color.DARK_SALMON : set = set_horizon_color
@@ -14,16 +17,9 @@ const SKY_SIZE = 64.0
 
 @onready var ground = $Ground
 @onready var ground_material : ShaderMaterial  = ground.get_active_material(0)
-@onready var sky_material : ShaderMaterial = $Sky.get_active_material(0)
 
 
-func set_sky_color(value):
-	sky_color = value
-	if !sky_material:
-		return
-	sky_material.set_shader_parameter("sky_color", value)
-	environment.background_color = value
-	
+
 
 func set_ground_color(value):
 	ground_color = value
@@ -31,25 +27,25 @@ func set_ground_color(value):
 		return
 	ground_material.set_shader_parameter("albedo", value)
 	ground_material.set_shader_parameter("emission", value)
-	sky_material.set_shader_parameter("ground_color", value)
-
+	%Sky.get_active_material(0).set_shader_parameter("ground_color", value)
+#
 func set_fog_color(value):
 	fog_color = value
-	if !sky_material:
+	if !%Sky.get_active_material(0):
 		return
 	environment.fog_color = value
-	sky_material.set_shader_parameter("fog_color", value)
+	%Sky.get_active_material(0).set_shader_parameter("fog_color", value)
 
-
+#
 func set_horizon_color(value):
 	horizon_color = value
-	if !sky_material:
+	if !%Sky.get_active_material(0):
 		return
-	sky_material.set_shader_parameter("horizon_color", value)
+	%Sky.get_active_material(0).set_shader_parameter("horizon_color", value)
 
 func set_horizon_height(value):
 	horizon_height = value
-	if !sky_material:
+	if !%Sky.get_active_material(0):
 		return
-	sky_material.set_shader_parameter("horizon_height", value / SKY_SIZE)
+	%Sky.get_active_material(0).set_shader_parameter("horizon_height", value / SKY_SIZE)
 	ground.position.y = value
