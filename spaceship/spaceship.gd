5d48924911d7e0d09853c8593942a4bcd16a829a
Adam Spruijt (Mousley) <adamspruijt@gmail.com>
2023-03-04 22:04:15 -0500

    cleanup

--- spaceship/spaceship.gd
+++ spaceship/spaceship.gd
@@ -1,8 +1,10 @@
+@tool
 extends Node3D
 
-@export var door_open := false : set = set_door_open
+@export var door_open := false : 
+	set(value):
+		door_open = value
+		var state_machine : AnimationNodeStateMachinePlayback = %DoorState.get("parameters/playback")
+		state_machine.travel( 'open' if door_open else 'close' ) 
 
 
-func set_door_open(value):
-	print(value)
-	door_open = value
