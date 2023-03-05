8d61e5358e703a7b2cdbb16621f4625293ac9326
Adam Spruijt (Mousley) <adamspruijt@gmail.com>
2023-03-05 16:04:12 -0500

    work

--- game_state_manager.gd
+++ game_state_manager.gd
@@ -1,11 +1,27 @@
+@tool
 extends AnimationTree
 
+@export_enum("intro","blast_off","stars_light_up","warp","landing") var mission_state: String = "intro":
+	set(value):
+		mission_state = value
+		change_state('mission_state', value)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@export_enum("intro","approach_spaceship","enter_spaceship") var player_state: String = "intro":
+	set(value):
+		player_state = value
+		change_state('player_state', value)
+
+@export_enum("intro","enter_spaceship","standing","sitting") var character_state: String = "intro":
+	set(value):
+		character_state = value
+		change_state('character_state', value)
+
+@export_enum("idle","buckle_warning","talking_head","success","failure","restart_button","visualizer") var screen_state: String = "intro":
+	set(value):
+		screen_state = value
+		change_state('screen_state', value)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func change_state(machine : String, state_name: String) -> void:
+	var state_machine = self.get("parameters/%s/playback" % [machine])
+	state_machine.travel(state_name)
