075abff75662d6bc8953f20aaa7d3a4fd94b0c64
Adam Spruijt (Mousley) <adamspruijt@gmail.com>
2023-03-16 21:38:55 -0400

    cleanup

--- audio/recorder/MicRecord.gd
+++ audio/recorder/MicRecord.gd
@@ -1,27 +1,26 @@
 extends Control
 
-var effect
-var recording
+var recording : AudioStreamWAV
 
-func _ready():
-	var idx = AudioServer.get_bus_index("Record")
-	effect = AudioServer.get_bus_effect(idx, 0)
+@onready var record_bus_index := AudioServer.get_bus_index("Record")
+@onready var effect : AudioEffectRecord = AudioServer.get_bus_effect(record_bus_index, 0)
+
 
 
 func _on_RecordButton_pressed():
 	if effect.is_recording_active():
 		recording = effect.get_recording()
-		$PlayButton.disabled = false
-		$SaveButton.disabled = false
+		%PlayButton.disabled = false
+		%SaveButton.disabled = false
 		effect.set_recording_active(false)
-		$RecordButton.text = "Record"
-		$Status.text = ""
+		%RecordButton.text = "Record"
+		%Status.text = ""
 	else:
-		$PlayButton.disabled = true
-		$SaveButton.disabled = true
+		%PlayButton.disabled = true
+		%SaveButton.disabled = true
 		effect.set_recording_active(true)
-		$RecordButton.text = "Stop"
-		$Status.text = "Recording..."
+		%RecordButton.text = "Stop"
+		%Status.text = "Recording..."
 
 
 func _on_PlayButton_pressed():
@@ -31,14 +30,14 @@ func _on_PlayButton_pressed():
 	print(recording.stereo)
 	var data = recording.get_data()
 	print(data.size())
-	$AudioStreamPlayer.stream = recording
-	$AudioStreamPlayer.play()
+	%AudioStreamPlayer.stream = recording
+	%AudioStreamPlayer.play()
 
 
 
 #
 #
 func _on_SaveButton_pressed():
-	var save_path = $SaveButton/Filename.text
+	var save_path = %SaveButton/Filename.text
 	recording.save_to_wav(save_path)
-	$Status.text = "Saved WAV file to: %s\n(%s)" % [save_path, ProjectSettings.globalize_path(save_path)]
+	%Status.text = "Saved WAV file to: %s\n(%s)" % [save_path, ProjectSettings.globalize_path(save_path)]
