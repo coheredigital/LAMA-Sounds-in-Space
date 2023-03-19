f31373d3ee63ef1fa3bc73cb6186e0eeec332969
Adam <adam@cohere.digital>
2023-03-19 14:17:03 -0400

    Imported stimuli

---
+++ data/stimuli_resource.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name StimuliResource
+
+@export var audio_file: AudioStreamWAV
+@export var sub_resource: Resource
+@export var strings: PackedStringArray
---
+++ ui/forms/stimulus_list.gd
@@ -0,0 +1,5 @@
+extends Control
+
+@export var stimuli = {
+	"name" 
+}
