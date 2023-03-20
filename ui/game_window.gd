09bdeba7b5f73cf0e80628aa564d22d5adeb2a45
Adam <adam@cohere.digital>
2023-03-19 21:39:33 -0400

    customizing dialog window

--- ui/game_window.gd
+++ ui/game_window.gd
@@ -1,7 +1,15 @@
 extends Window
 
+@onready var dialog_balloon = %DialogueBalloon
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var resource = load("res://dialog/intro.dialogue")
-	DialogueManager.show_example_dialogue_balloon(resource, "start")
+	show_dialogue_balloon(resource, "start")
+
+
+func show_dialogue_balloon(resource: DialogueResource, title: String = "0", extra_game_states: Array = []) -> void:
+	var BalloonScene = load("res://ui/dialogue_balloon/dialogue_balloon.tscn")
+	var balloon: Node = BalloonScene.instantiate()
+	add_child(balloon)
+	balloon.start(resource, title, extra_game_states)
