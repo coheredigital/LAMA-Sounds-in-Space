52abf360e6922c6a5ab98f044e3f0fa198d3582a
Adam <adam@cohere.digital>
2023-03-19 21:43:25 -0400

    minor refactoring

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -6,13 +6,16 @@ extends Node
 @export_enum("intro","approach_spaceship","boarding","enter_spaceship","sitting") var state: String = "intro":
 	set(value):
 		state = value
-		if %PositionStateTree:
-			var state_machine = %PositionStateTree.get("parameters/playback")
+		if position_state_tree:
+			var state_machine = position_state_tree.get("parameters/playback")
 			state_machine.travel(state)
+
 @export_range (1.0, 8.0) var follow_speed = 3.0
 @export_range (1.0, 8.0) var turn_speed = 4.0
 @export_range (0.0, 10.0) var distance_buffer = 1.0
 @export_range (1.0, 10.0) var distance_boost = 2.0
+
+@onready var position_state_tree = %PositionStateTree
 @onready var target : Marker3D = $PositionTarget
 @onready var look_target : Marker3D = $LookTarget
 @onready var camera := $Camera3D
