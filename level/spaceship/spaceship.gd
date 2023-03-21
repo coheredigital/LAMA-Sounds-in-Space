58cb52e30f5d369447f8000e6505779f50b33bbc
Adam <adam@cohere.digital>
2023-03-20 21:35:35 -0400

    fuel level control handled by global shader param

--- level/spaceship/spaceship.gd
+++ level/spaceship/spaceship.gd
@@ -1,7 +1,7 @@
 @tool
 extends Node3D
 
-@export_enum("idle","wipe","computer","start_button","restart_button","success", "failure", "buckle_warning") var screen_state: String = "intro":
+@export_enum("idle","wipe","alert","computer","start_button","restart_button","success", "failure", "buckle_warning") var screen_state: String = "intro":
 	set(value):
 		screen_state = value
 		update_screen(value)
@@ -20,18 +20,24 @@ extends Node3D
 @export_range(0.0,1.0) var steering_motion := 0.0 : 
 	set(value):
 		steering_motion = value
-		if animation_tree:
-			animation_tree.set("parameters/steering_speed/scale", steering_motion)
+		set_steering_motion(value)
+
 
 @export_range(0.0,1.0) var fuel_level := 0.0 : 
 	set(value):
 		fuel_level = value
+		RenderingServer.global_shader_parameter_set("fuel_level", value)
 		
 @onready var animation_tree := %AnimationTree
 
 func _ready():
 	Sequencer.screen_changed.connect(update_screen)
 	Sequencer.door_open_changed.connect(set_door_open)
+	Sequencer.steering_motion_changed.connect(set_steering_motion)
+
+func set_steering_motion(value: float) -> void:
+	if animation_tree:
+		animation_tree.set("parameters/steering_speed/scale", value)
 
 func set_door_open(value: bool) -> void:
 	if animation_tree:
--- sequencer.gd
+++ sequencer.gd
@@ -1,17 +1,20 @@
 extends Node
 
-signal mission_changed(value: String)
+signal dialogue_visibility_changed(value: bool)
 signal level_changed(value: String)
-signal screen_changed(value: String)
 signal player_state_changed(value: String)
 signal character_state_changed(value: String)
+signal spaceship_motion_changed(value: float)
 signal door_open_changed(value: bool)
-signal balloon_visibility_changed(value: bool)
+signal seatbelt_status_changed(value: bool)
+signal screen_changed(value: String)
+signal steering_motion_changed(value: float)
+signal fuel_level_changed(value: float)
 
-@export var mission: String = "intro":
+@export var dialogue_visible: bool = false:
 	set(value):
-		mission = value
-		mission_changed.emit(value)
+		dialogue_visible = value
+		dialogue_visibility_changed.emit(value)
 
 @export var character_state: String = "intro":
 	set(value):
@@ -23,18 +26,35 @@ signal balloon_visibility_changed(value: bool)
 		level = value
 		level_changed.emit(value)
 
-@export var screen: String = "idle":
-	set(value):
-		screen = value
-		screen_changed.emit(value)
-
-@export var door_open: bool = false:
-	set(value):
-		door_open = value
-		door_open_changed.emit(value)
-
 @export var player_state: String = "intro":
 	set(value):
 		player_state = value
 		player_state_changed.emit(value)
 		
+@export_range(0.0,1.0) var spaceship_motion: float = 0.0:
+	set(value):
+		spaceship_motion = value
+		spaceship_motion_changed.emit(value)
+
+@export_range(0.0,1.0) var steering_motion: float = 0.0:
+	set(value):
+		steering_motion = value
+		steering_motion_changed.emit(value)
+		
+@export var door_open: bool = false:
+	set(value):
+		door_open = value
+		door_open_changed.emit(value)
+		
+@export var seatbelt_status: bool = false:
+	set(value):
+		seatbelt_status = value
+		seatbelt_status_changed.emit(value)
+		
+@export var screen: String = "idle":
+	set(value):
+		screen = value
+		screen_changed.emit(value)
+
+
+
