08607bbbc458b2dfb268deb52c792e560b5afcfc
Adam <adam@cohere.digital>
2023-04-15 19:09:37 -0400

    minor updates

--- components/remote_follow_path.gd
+++ components/remote_follow_path.gd
@@ -1,15 +1,26 @@
 @tool
 extends Path3D
 
-@export_range (0.0, 1.0, 0.01) var progress := 0.0 :
+@export_range (0.0, 64.0, 0.1) var progress := 0.0 :
 	set(value):
 		progress = value
-		update_postion(value)
+		update_progress(value)
+
+@export_range (0.0, 1.0, 0.01) var progress_ratio := 0.0 :
+	set(value):
+		progress_ratio = value
+		update_progress_ratio(value)
+
 
 @onready var path_follow : PathFollow3D = $PathFollow
 @onready var remote_transform := $PathFollow/RemoteTransform3D
 
-func update_postion(value: float, duration: float = 2.0) -> void:
+func update_progress(value: float, duration: float = 2.0) -> void:
+	if path_follow:
+		var tween = create_tween()
+		tween.tween_property(path_follow, "progress", value, duration).set_trans(Tween.TRANS_SINE)
+
+func update_progress_ratio(value: float, duration: float = 2.0) -> void:
 	if path_follow:
 		var tween = create_tween()
 		tween.tween_property(path_follow, "progress_ratio", value, duration).set_trans(Tween.TRANS_SINE)
