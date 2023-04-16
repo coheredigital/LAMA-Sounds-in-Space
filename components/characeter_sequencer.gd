0a561295040dc9c0393cb4ceb9143dfaff4da617
Adam <adam@cohere.digital>
2023-04-16 16:04:50 -0400

    new separate character sequencer

---
+++ components/characeter_sequencer.gd
@@ -0,0 +1,30 @@
+extends Node
+
+
+
+signal position_changed(value: float, duration: float)
+signal action_changed(value: String)
+signal view_angle_changed(value: Vector2)
+
+var position: float = 0.0:
+	set(value):
+		position = value
+		position_changed.emit(value, 0.0)
+
+var view_angle: Vector2 = Vector2(0.0,0.0):
+	set(value):
+		view_angle = value
+		view_angle_changed.emit(value, 0.0)
+
+var action: String = "idle":
+	set(value):
+		action = value
+		action_changed.emit(value)
+
+
+
+func move(progress: float, duration: float = 1.0) -> void:
+	position_changed.emit(progress, duration)
+
+func view_angle(angle: Vector2, duration: float = 1.0) -> void:
+	view_angle_changed.emit(angle, duration)
