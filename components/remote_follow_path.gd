2dcd9d552cef525f7a1c15cc8406c54423cd69a4
Adam <adam@cohere.digital>
2023-04-16 15:17:56 -0400

    refactoring character path controls

--- components/remote_follow_path.gd
+++ components/remote_follow_path.gd
@@ -1,10 +1,13 @@
 @tool
 extends Path3D
 
+const LOOK_ANGLE_LIMIT = Vector2(40.0,40.0)
+
 @export_range (0.0, 64.0, 0.1) var progress := 0.0 :
 	set(value):
 		progress = value
 		update_progress(value)
+
 
 @export_range (0.0, 1.0, 0.001) var progress_ratio := 0.0 :
 	set(value):
@@ -12,7 +15,21 @@ extends Path3D
 		update_progress_ratio(value)
 
 
+@export_range (0.0, 2.0, 0.1) var look_height := 1.0:
+	set(value):
+		look_height = value
+		
+		if look_target:
+			look_target.position.y = look_height
+
+
+@export var look_angle := Vector2(0.0,0.0):
+	set(value):
+		look_angle = clamp(value, Vector2(-1.0,-1.0), Vector2(1.0,1.0))
+		update_look_angle(look_angle)
+
 @onready var path_follow : PathFollow3D = $PathFollow
+@onready var look_target : Marker3D = %LookTarget
 @onready var remote_transform := $PathFollow/RemoteTransform3D
 
 func update_progress(value: float, duration: float = 2.0) -> void:
@@ -26,3 +43,10 @@ func update_progress_ratio(value: float, duration: float = 2.0) -> void:
 		tween.tween_property(path_follow, "progress_ratio", value, duration).set_trans(Tween.TRANS_SINE)
 
 
+func update_look_angle(value: Vector2, duration: float = 2.0) -> void:
+	if look_target:
+		var tween = create_tween()
+		var target_angle = Vector3(0.0,0.0,0.0)
+		target_angle.x = value.x * LOOK_ANGLE_LIMIT.x
+		target_angle.x = value.y * LOOK_ANGLE_LIMIT.y
+		tween.tween_property(look_target, "rotation_degrees", target_angle, duration).set_trans(Tween.TRANS_SINE)
--- level/level.gd
+++ level/level.gd
@@ -6,12 +6,19 @@ var state: String = "intro":
 		update_state(value)
 
 @onready var state_tree := %StateTree
+@onready var character_path := %Character
 
 func _ready():
 	Sequencer.level_changed.connect(update_state)
+	Sequencer.character_position_changed.connect(update_character_postion)
+	
 	
 func update_state(value: String) -> void: 
 	if state_tree:
 		var state_machine : AnimationNodeStateMachinePlayback = state_tree.get("parameters/playback")
 		state_machine.travel(value)
 		
+func update_character_postion(value: float, duration: float = 2.0) -> void: 
+	if character_path:
+		var tween = create_tween()
+		tween.tween_property(character_path, "progress_ratio", value, duration).set_trans(Tween.TRANS_SINE)
