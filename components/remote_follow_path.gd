dee3196713739963cee804e4de243db8c59212ab
Adam <adam@cohere.digital>
2023-04-17 20:11:15 -0400

    fixed offset issue with player camera path

--- components/remote_follow_path.gd
+++ components/remote_follow_path.gd
@@ -1,7 +1,7 @@
 @tool
 extends Path3D
 
-const LOOK_ANGLE_LIMIT = Vector2(15.0,-90.0)
+const LOOK_ANGLE_LIMIT = Vector2(20.0,-180.0)
 
 @export_range (0.0, 64.0, 0.1) var progress := 0.0 :
 	set(value):
@@ -30,40 +30,28 @@ const LOOK_ANGLE_LIMIT = Vector2(15.0,-90.0)
 
 @onready var path_follow : PathFollow3D = %PathFollow
 @onready var look_target : Node3D = %LookTarget
+@onready var remote_transform : Node3D = %RemoteTransform
 
 @export_range (4.0, 16.0, 0.1) var follow_speed := 16.0
 @export_range (4.0, 16.0, 0.1) var turn_speed := 16.0
 @export_range (0.1, 2.0, 0.1) var max_distance := 0.5
 
 
-func _ready():
-	
-	if not %RemoteTransform:
-		return
-	if not max_distance:
-		push_warning("Max Distance not set on: %s" % [self] )
-	if not follow_speed:
-		push_warning("Follow Speed not set on: %s" % [self] )
-
 func _process(delta):
-	
-	if not %RemoteTransform or not max_distance or not follow_speed:
-		return
-
-	var distance = %RemoteTransform.position.distance_to(path_follow.global_transform.origin)
+	var distance = remote_transform.position.distance_to(path_follow.position)
 	var distance_ratio = clamp(smoothstep(0.0, max_distance, distance), 0.0,1.0)
-	if distance_ratio > 0.01:
+	if distance > 0.01:
 		var follow_distance_speed = lerp(0.1, follow_speed, distance_ratio);
-		%RemoteTransform.position = %RemoteTransform.position.move_toward(path_follow.global_transform.origin, delta * follow_distance_speed)
-	var look_transform = %RemoteTransform.global_transform.looking_at(look_target.global_transform.origin, Vector3.UP)
-	%RemoteTransform.global_transform = %RemoteTransform.global_transform.interpolate_with(look_transform, delta * turn_speed)
-
+		remote_transform.position = remote_transform.position.move_toward(path_follow.global_transform.origin, delta * follow_distance_speed)
+	var look_transform = remote_transform.global_transform.looking_at(look_target.global_transform.origin, Vector3.UP)
+	remote_transform.global_transform = remote_transform.global_transform.interpolate_with(look_transform, delta * turn_speed)
 
 
 func update_progress(value: float, duration: float = 1.0) -> void:
 	if path_follow:
 		var tween = create_tween()
 		tween.tween_property(path_follow, "progress", value, duration).set_trans(Tween.TRANS_SINE)
+
 
 func update_progress_ratio(value: float, duration: float = 1.0) -> void:
 	if path_follow:
