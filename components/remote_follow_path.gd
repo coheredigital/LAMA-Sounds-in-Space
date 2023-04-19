f8746c905a2465c815f3b7fe0725987f357b2972
Adam <adam@cohere.digital>
2023-04-18 21:40:24 -0400

    refactoring

--- components/remote_follow_path.gd
+++ components/remote_follow_path.gd
@@ -30,10 +30,10 @@ const LOOK_ANGLE_LIMIT = Vector2(20.0,-180.0)
 
 @export_range (1.0, 8.0, 0.1) var follow_speed := 4.0
 @export_range (1.0, 8.0, 0.1) var turn_speed := 4.0
-@export_range (1.0, 8.0, 0.1) var max_distance := 0.5
+@export_range (1.0, 4.0, 0.1) var max_distance := 2.0
 
 
-func _process(delta):
+func _physics_process(delta):
 	var distance = remote_transform.position.distance_to(path_follow.position)
 	var distance_ratio = clamp(smoothstep(0.0, max_distance, distance), 0.0,1.0)
 	if distance > 0.01:
@@ -46,13 +46,13 @@ func _process(delta):
 func update_progress(value: float, duration: float = 1.0) -> void:
 	if path_follow:
 		var tween = create_tween()
-		tween.tween_property(path_follow, "progress", value, duration).set_trans(Tween.TRANS_SINE)
+		tween.tween_property(path_follow, "progress", value, duration).set_trans(Tween.TRANS_LINEAR)
 
 
 func update_progress_ratio(value: float, duration: float = 1.0) -> void:
 	if path_follow:
 		var tween = create_tween()
-		tween.tween_property(path_follow, "progress_ratio", value, duration).set_trans(Tween.TRANS_SINE)
+		tween.tween_property(path_follow, "progress_ratio", value, duration).set_trans(Tween.TRANS_LINEAR)
 
 
 func update_view_angle(value: Vector2, duration: float = 1.0) -> void:
@@ -62,4 +62,4 @@ func update_view_angle(value: Vector2, duration: float = 1.0) -> void:
 		target_angle.x = value.x * LOOK_ANGLE_LIMIT.x
 		target_angle.y = value.y * LOOK_ANGLE_LIMIT.y
 		if tween:
-			tween.tween_property(%LookTargetPivot, "rotation_degrees", target_angle, duration).set_trans(Tween.TRANS_SINE)
+			tween.tween_property(%LookTargetPivot, "rotation_degrees", target_angle, duration).set_trans(Tween.TRANS_LINEAR)
