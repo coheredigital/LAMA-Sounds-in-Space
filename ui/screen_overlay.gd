62b7ba5b238d07d385115614412104f302b0f68b
Adam <adam@cohere.digital>
2023-04-30 18:44:18 -0400

    added screen overlay to game window

---
+++ ui/screen_overlay.gd
@@ -0,0 +1,19 @@
+@tool
+extends ColorRect
+
+@onready var overlay_material := self.material
+
+@export var active = false:
+	set(value):
+		active = value
+		
+		var tween_blur = create_tween()
+		var tween_mix = create_tween()
+		
+		if active:
+			tween_blur.tween_property(overlay_material, "shader_parameter/blur_amount", 3.6, 3.0).set_trans(Tween.TRANS_EXPO)
+			tween_mix.tween_property(overlay_material, "shader_parameter/mix_amount", 0.7, 3.0).set_trans(Tween.TRANS_EXPO)
+		else:
+			tween_blur.tween_property(overlay_material, "shader_parameter/blur_amount", 0.0, 3.0).set_trans(Tween.TRANS_EXPO)
+			tween_mix.tween_property(overlay_material, "shader_parameter/mix_amount", 0.0, 3.0).set_trans(Tween.TRANS_EXPO)
+#
