61b884c1ef2cabc84fbe3efb70a9defb24403af4
Adam <adam@cohere.digital>
2023-05-04 20:33:14 -0400

    stimuli player works

--- audio/stimuli_player.gd
+++ audio/stimuli_player.gd
@@ -1,13 +1,21 @@
 extends Node
 
+@onready var player = $AudioStreamPlayer
+
 @export_dir var folder
+
 
 func play(sentence_id: String) -> void:
 	var filename = "%s/%s.wav" % [folder,sentence_id];
 	
 	var wav_file = load(filename);
 	
-	if wav_file:
-		print(wav_file)
-	else:
+	if not wav_file:
 		push_warning('Stilumi not found: %s' % [filename])
+		return
+	
+	print('Stimuli: %s' % [filename])
+
+	player.stream = wav_file
+	player.playing = true
+		
