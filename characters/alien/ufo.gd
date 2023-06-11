ed6961bdecbf21142dba696d90969347c31499be
Adam <adam@cohere.digital>
2023-06-11 13:46:21 -0400

    work

--- characters/alien/ufo.gd
+++ characters/alien/ufo.gd
@@ -14,12 +14,12 @@ func _ready():
 		beam_transform.top_level = true
 	
 func _process(delta):
-#	var distance = beam_transform.position.distance_to(beam_transform_target.position)
-#	var distance_ratio = clamp(smoothstep(0.0, 1.0, distance), 0.0,1.0)
+	var distance = beam_transform.position.distance_to(beam_transform_target.position)
+	var distance_ratio = clamp(smoothstep(0.0, 1.0, distance), 0.0,1.0)
 #	if distance > 0.01:
 #		var follow_speed = lerp(0.1, 4.0, distance_ratio);
 #		beam_transform.position = beam_transform.position.move_toward(beam_transform_target.global_transform.origin, delta * follow_speed)
-		
+
 	if beam:
 		var look_transform = beam.global_transform.looking_at(beam_transform.global_transform.origin, Vector3( 0,0,1))
 		beam.global_transform = beam.global_transform.interpolate_with(look_transform, delta * turn_speed)
