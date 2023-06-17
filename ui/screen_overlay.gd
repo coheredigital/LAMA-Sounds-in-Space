e47d72af4090a92e498d138057cad2cdd3ed1155
Adam <adam@cohere.digital>
2023-06-17 18:44:50 -0400

    screen overlay improvments

--- ui/screen_overlay.gd
+++ ui/screen_overlay.gd
@@ -3,33 +3,34 @@ extends ColorRect
 
 
 
-@onready var overlay_material := self.material
-@onready var logo := %Logo
+@onready var overlay_material := load("res://ui/screen_overlay_material.tres")
+
 
 @export_enum("hidden","logo","blackout") var state : String = "hidden":
 	set(value):
 		state = value
+		
 		var tween = create_tween()
 		var tween_logo = create_tween()
 		
-		if not tween or not tween_logo:
+		if not tween:
 			return
 		
-		match state:
+		match value:
 			"hidden":
-				tween_logo.tween_property(logo, "modulate:a", 0.0, 2.0).set_trans(Tween.TRANS_SINE)
+				tween_logo.tween_property(%Logo, "modulate:a", 0.0, 2.0).set_trans(Tween.TRANS_SINE)
 				tween.parallel().tween_property(overlay_material, "shader_parameter/blur_amount", 0.0, 2.0).set_trans(Tween.TRANS_SINE)
 				tween.parallel().tween_property(overlay_material, "shader_parameter/mix_amount", 0.0, 2.0).set_trans(Tween.TRANS_SINE)
 			"logo":
 				tween.parallel().tween_property(overlay_material, "shader_parameter/blur_amount", 3.6, 2.0).set_trans(Tween.TRANS_SINE)
 				tween.parallel().tween_property(overlay_material, "shader_parameter/mix_amount", 0.7, 2.0).set_trans(Tween.TRANS_SINE)
 				tween_logo.tween_interval(1.5)
-				tween_logo.tween_property(logo, "modulate:a",  1.0, 1.0).set_trans(Tween.TRANS_SINE)
-			"black":
+				tween_logo.tween_property(%Logo, "modulate:a",  1.0, 1.0).set_trans(Tween.TRANS_SINE)
+			"blackout":
 				tween.parallel().tween_property(overlay_material, "shader_parameter/blur_amount", 6.0, 2.0).set_trans(Tween.TRANS_SINE)
 				tween.parallel().tween_property(overlay_material, "shader_parameter/mix_amount", 1.0, 2.0).set_trans(Tween.TRANS_SINE)
 				tween_logo.tween_interval(1.5)
-				tween_logo.tween_property(logo, "modulate:a",  0.0, 1.0).set_trans(Tween.TRANS_SINE)
+				tween_logo.tween_property(%Logo, "modulate:a",  0.0, 1.0).set_trans(Tween.TRANS_SINE)
 				
 
 
